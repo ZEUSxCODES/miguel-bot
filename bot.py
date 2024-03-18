03f602b0c0800141d5eed4833d05a6df55851024
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-18 17:25:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import logging
 from pyrogram import Client, filters, idle
 from sample_config import Config
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = Client(
    "NoPMsBot",
@@ -37,7 +38,7 @@ async def help_command(_, message):
         "Why are you here❓\n\n"
         "➥ Inquiries 🔦\n"
         "➥ Doubts 🤔\n"
-        "➥ Problems 😰\n"
+        "➦ Problems 😰\n"
         "➥ Help 😟\n"
         "➥ Feedbacks 🔰\n"
         "➥ Promotion 🛒\n"
@@ -52,13 +53,37 @@ async def help_command(_, message):
     )
     await message.reply_text(help_text)
 
+@bot.on_message(filters.command("donate") & filters.private)
+async def donate_command(_, message):
+    donate_message = (
+        "❤️ Thanks for showing interest in donation 😟\n\n"
+        "Donate us to keep our services continuously alive 😢\n"
+        "You can send any amount\n"
+        "of 10₹, 20₹, 30₹, 50₹, 70₹, 100₹, 200₹ ...as you wish 😊\n\n"
+        "📨 Payment methods 💳\n"
+        "GooglePay / Paytm / PhonePay / Net Banking ...\n\n"
+        "For donation message me💬 \n"
+        "@anmol0700 [or here via this bot]\n\n"
+        "Or you can scan the QR code below 👇\n"
+        "UPI link 🔗 also there 😇\n\n"
+        "Thanking you 🌹\n\n"
+        "🛍 UPI ID:\n<code>anmol0700@fam</code>"
+    )
+    
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton('💳 Donate 💳', url='https://te.legra.ph/Donate-Us-03-15')]
+    ])
+
+    await message.reply_text(donate_message, reply_markup=keyboard, parse_mode='html')
+
 @bot.on_message(filters.text | 
                 filters.media | 
                 filters.sticker | 
                 filters.animation | 
                 filters.private | 
                 ~filters.command("start") &
-                ~filters.command("help"))
+                ~filters.command("help") &
+                ~filters.command("donate"))
 async def send_func(_, message):
     user_id = message.from_user.id
     if user_id == Config.OWNER_ID:
