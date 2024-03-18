d61ecb1c04f12574a38497ea0a5e6939f70f6cdc
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-18 18:37:04 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6813230293:AAEzC1y8PyZMVMFCxQ9O_hkVISXrcLTD-rs")
 
-    APP_ID = int(os.environ.get("APP_ID", ))
+    APP_ID = int(os.environ.get("APP_ID", 20434292))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "ea4683f64ac46aa1d6fb236638a0ac01")
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1663603208))
     
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "@Anmol0700")
