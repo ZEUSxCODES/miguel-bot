f7c6521c74be025e7e8dfff1b82400dd02649c17
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-18 18:40:57 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6813230293:AAEzC1y8PyZMVMFCxQ9O_hkVISXrcLTD-rs")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6729640583:AAHjLJzyTwR4hFDGpajET-zwCd0H-cjTNFM")
 
     APP_ID = int(os.environ.get("APP_ID", 20434292))
 
