07b414561ac00b77f4cc8712faa08a83daa086d8
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-18 17:54:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ from sample_config import Config
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = Client(
-   "NoPMsBot",
-   api_id=Config.APP_ID,
-   api_hash=Config.API_HASH,
-   bot_token=Config.BOT_TOKEN,
+    "NoPMsBot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
 )
 
 @bot.on_message(filters.command("start") & filters.private)
@@ -22,7 +22,7 @@ async def start(_, message):
             f"Hi {mention}\n\n"
             "How are you today?\n"
             "I can deliver your messages to my boss.\n"
-            "Just leave your messages & wait for reply.\n"
+            "Just leave your messages & wait for a reply.\n"
             "Don't try to spam, else you'll be blocked instantly.\n"
             "I've notified my boss that you've started me!\n\n"
             "Now tell me why you came here?\n"
@@ -72,17 +72,17 @@ async def help_command(_, message):
 async def donate_command(_, message):
     donate_message = (
         "❤️ Thanks for showing interest in donation 😟\n\n"
-        "Donate us to keep our services continuously alive 😢\n"
+        "Donate to keep our services continuously alive 😢\n"
         "You can send any amount\n"
         "of 10₹, 20₹, 30₹, 50₹, 70₹, 100₹, 200₹ ...as you wish 😊\n\n"
         "📨 Payment methods 💳\n"
         "GooglePay / Paytm / PhonePay / Net Banking ...\n\n"
-        "For donation message me💬 \n"
+        "For donation, message me💬 \n"
         "@anmol0700 [or here via this bot]\n\n"
         "Or you can scan the QR code below 👇\n"
         "UPI link 🔗 also there 😇\n\n"
         "Thanking you 🌹\n\n"
-        "🛍 UPI ID:\n<code>anmol0700@fam</code>"
+        "🛍 UPI ID:\n`anmol0700@fam`"
     )
     
     keyboard = InlineKeyboardMarkup([
@@ -91,7 +91,22 @@ async def donate_command(_, message):
          InlineKeyboardButton('🔄 Update Channel', url='https://t.me/Film_Nest')]
     ])
 
-    await message.reply_text(donate_message, reply_markup=keyboard, parse_mode='html')
+    await message.reply_text(donate_message, reply_markup=keyboard, parse_mode='markdown')
+
+@bot.on_message(filters.command("broadcast") & filters.private & filters.user(Config.OWNER_ID))
+async def broadcast_command(_, message):
+    if message.reply_to_message:
+        sent_message = message.reply_to_message
+        sent_message_text = sent_message.text or sent_message.caption or ""
+        
+        async for user in bot.iter_users():
+            try:
+                await bot.send_message(user.id, sent_message_text)
+            except Exception as e:
+                logging.error(f"Error broadcasting message to {user.id}: {e}")
+        await message.reply_text("Broadcast sent successfully!")
+    else:
+        await message.reply_text("Please reply to a message to broadcast.")
 
 @bot.on_message(filters.text | 
                 filters.media | 
@@ -100,21 +115,13 @@ async def donate_command(_, message):
                 filters.private | 
                 ~filters.command("start") &
                 ~filters.command("help") &
-                ~filters.command("donate"))
+                ~filters.command("donate") &
+                ~filters.command("broadcast"))
 async def send_func(_, message):
-    user_id = message.from_user.id
-    if user_id == Config.OWNER_ID:
-        if message.reply_to_message and message.reply_to_message.forward_from:
-            forward_user_id = message.reply_to_message.forward_from.id
-            try:
-                await bot.send_message(chat_id=forward_user_id, text=message.text)
-            except Exception as e:
-                return await message.reply(str(e))
-    else:
-        try:
-            await message.forward(chat_id=Config.OWNER_ID)
-        except Exception as e:
-            return await message.reply(str(e))
+    try:
+        await message.forward(chat_id=Config.OWNER_ID)
+    except Exception as e:
+        logging.error(f"Error forwarding message to owner: {e}")
 
 bot.start()
 idle()
